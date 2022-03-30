8f351abcdb6b95953a79bbcab3be61bdf664f3dd
manjitsingh-yadav <85680748+manjitsingh-yadav@users.noreply.github.com>
2022-03-31 00:04:06 +0530

    Update etcd-snapshot.sh

--- Task5/etcd-snapshot.sh
+++ Task5/etcd-snapshot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ETCDCTL_API=3 etcdctl --endpoints 172.31.18.43:2379 --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt snapshot save /tmp/snapshot-db-$(date +%d-%m-%y-%H-%M)
+sudo ETCDCTL_API=3 etcdctl --endpoints 127.0.0.1:2379 --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt snapshot save /tmp/snapshot-db-$(date +%d-%m-%y-%H-%M)
